1f4b651824c532b01fe1c8b8f2d563da89c82a50
myrrhpansies <131057843+myrrhpansies@users.noreply.github.com>
2023-05-28 08:53:05 -0700

    buttons

--- Scenes/test_scene.gd
+++ Scenes/test_scene.gd
@@ -1,33 +1,41 @@
 extends Node2D
 var notes = 0
-var testArray = [1,2,3]
+var testArray = [1,1,1,1]
 var inputArray = []
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
-	pass # Replace with function body.
+	print(testArray.slice(0,2))
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 func arrowPress():
 	if Input.is_action_just_pressed("leftArrow"):
-		inputArray.push_front(1)
+		inputArray.push_back(1)
 		notes += 1
 		print(inputArray)
-	elif 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	elif Input.is_action_just_pressed("rightArrow"):
+		inputArray.push_back(2)
+		notes += 1	
+		print(inputArray)
+	elif Input.is_action_just_pressed("downArrow"):
+		inputArray.push_back(3)
+		notes += 1	
+		print(inputArray)
+
 func checkNotes():
 	if notes == 0:
 		pass
 	elif notes == 1:
-		if testArray[0] == inputArray[0]:
-			pass
-		else:
-			print("you lose")	
-	elif notes == 2:
-		pass
-	elif notes == 3:
-		pass				
+		if testArray[0] != inputArray[0]:
+			notes = 0
+			print("you lose")
+			inputArray = []
+	elif testArray.slice(0,notes) != inputArray.slice(0,notes):
+			notes = 0
+			print("you lose")
+			inputArray = []
+		
 func _process(delta):
 	arrowPress()
 	checkNotes()
+	
 
-func _on_sun_focus_entered():
-	pass # Replace with function body.
